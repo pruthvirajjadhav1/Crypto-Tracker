1a41d8a3d072b8c1307487607bf2faf5b6c0508d
Igor Zhukovich <igrzhukovich@gmail.com>
2022-10-01 14:31:35 +0300

    feature: added linter

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    },
+    "settings": {
+        "react": {
+          "version": "detect"
+        }
+      }
+}
